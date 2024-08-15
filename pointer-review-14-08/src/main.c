ecdb6616d7795422c070da25ba485ccda00566b9
Antonio Natusch <anthony2004951@gmail.com>
2024-08-15 17:15:52 -0400

    hw done

--- pointer-review-14-08/src/main.c
+++ pointer-review-14-08/src/main.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 
+int min(int a, int b) { return a > b ? b : a; }
+int max(int a, int b) { return a < b ? b : a; }
 void FindMinAndMax(int a, int b, int *pMax, int *pMin) {
 
   if (a > b) {
@@ -11,13 +13,14 @@ void FindMinAndMax(int a, int b, int *pMax, int *pMin) {
   }
 }
 
-int FindMaxOfThreeNumbers(int *list) {
+int FindMaxOfThreeNumbers(int a, int b, int c) {
+  if (a >= b && a >= c)
+    return a;
+  else if (b >= a && b >= c)
+    return b;
 
-
-  return max;
+  return c;
 }
-
-int min(int a, int b) { return a > b ? b : a; }
 
 void swap(int *a, int *b) { // solo de esta forma es que se pueden cambiar los
                             // valores con una funcion
@@ -29,9 +32,8 @@ void swap(int *a, int *b) { // solo de esta forma es que se pueden cambiar los
 }
 
 int main(int argc, char *argv[]) {
-  int a = 10, b = 15;
+  int a = 10, b = 15, c = 20;
   int *ptrMax, *ptrMin;
-
   /*  printf("%d\n", a);
     printf("%d\n", b);
     swap(&a, &b);
@@ -45,8 +47,7 @@ int main(int argc, char *argv[]) {
 
   printf("%d\n", *ptrMax);
   printf("%d\n", *ptrMin);
-
-
+  printf("%d\n", FindMaxOfThreeNumbers(a, b, c));
 
   return 0;
 }
