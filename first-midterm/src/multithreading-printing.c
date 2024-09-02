a8668c9cdb84b101ef8c934fb43f42eda8614606
Antonio Natusch <anthony2004951@gmail.com>
2024-09-02 16:56:59 -0400

    todo: create threads and print the ids

--- first-midterm/src/multithreading-printing.c
+++ first-midterm/src/multithreading-printing.c
@@ -1,7 +1,11 @@
 #include <pthread.h>
 #include <stdio.h>
-int main(int argc, char *argv[])
-{
+#define MAX_THREADS 5
+pthread_t threads[MAX_THREADS];
+
+void thr_creation(void *p);  // p = param cualquiera
+
+int main(int argc, char *argv[]) {
 
   printf("Hello World!\n");
   return 0;
