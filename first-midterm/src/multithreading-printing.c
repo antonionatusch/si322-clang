12fad3bd0fabd1bd3e2b586827eae9e43cc01268
Antonio Natusch <anthony2004951@gmail.com>
2024-09-04 16:18:31 -0400

    added a better version

--- first-midterm/src/multithreading-printing.c
+++ first-midterm/src/multithreading-printing.c
@@ -1,5 +1,6 @@
 #include <pthread.h>
 #include <stdio.h>
+#include <stdlib.h>
 #define MAX_THREADS 5
 pthread_t threads[MAX_THREADS];
 
@@ -7,28 +8,29 @@ typedef struct thr_params {
   int id;
 } thr_params;
 
-void thr_creation(void *p) // p = param cualquiera
-{
-  thr_params *data;
-  data = (thr_params *)p;
-  pthread_exit(&data->id);
+void *thr_creation(void *p) {
+  int *id = (int *)p;
+  pthread_exit(id); // Sale del hilo devolviendo el puntero al ID
 }
-
 int main(int argc, char *argv[]) {
   int *res;
   int n;
   void *r;
   printf("Enter a number of threads: ");
   scanf("%d", &n);
-  thr_params params[n];
+
   for (int i = 0; i < n; ++i) {
-    params[i].id = i + 1;
-    pthread_create(&threads[i], NULL, (void *)thr_creation, (void *)&params[i]);
+    int *id = malloc(sizeof(int)); // Asigna memoria para cada ID
+    *id = i + 1;
+    pthread_create(&threads[i], NULL, thr_creation, id);
   }
+
   for (int i = n - 1; i >= 0; --i) {
     pthread_join(threads[i], &r);
     res = (int *)r;
     printf("%d\n", *res);
+    free(res); // Libera la memoria después de usar el ID
   }
+
   return 0;
 }
