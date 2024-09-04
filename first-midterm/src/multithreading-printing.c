669562067e7235b0fad88621b37511f5e5d72492
Antonio Natusch <anthony2004951@gmail.com>
2024-09-04 13:36:25 -0400

    threads hw done

--- first-midterm/src/multithreading-printing.c
+++ first-midterm/src/multithreading-printing.c
@@ -3,10 +3,32 @@
 #define MAX_THREADS 5
 pthread_t threads[MAX_THREADS];
 
-void thr_creation(void *p);  // p = param cualquiera
+typedef struct thr_params {
+  int id;
+} thr_params;
+
+void thr_creation(void *p) // p = param cualquiera
+{
+  thr_params *data;
+  data = (thr_params *)p;
+  pthread_exit(&data->id);
+}
 
 int main(int argc, char *argv[]) {
-
-  printf("Hello World!\n");
+  int *res;
+  int n;
+  void *r;
+  printf("Enter a number of threads: ");
+  scanf("%d", &n);
+  thr_params params[n];
+  for (int i = 0; i < n; ++i) {
+    params[i].id = i + 1;
+    pthread_create(&threads[i], NULL, (void *)thr_creation, (void *)&params[i]);
+  }
+  for (int i = n - 1; i >= 0; --i) {
+    pthread_join(threads[i], &r);
+    res = (int *)r;
+    printf("%d\n", *res);
+  }
   return 0;
 }
